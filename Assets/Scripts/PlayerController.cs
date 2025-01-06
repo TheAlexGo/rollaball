1bbc5e2271399e15af1c7d8d2671a91dbe023786
Aleksandr Gordeev <alexander.gordeev@vk.team>
2025-01-06 21:00:39 +0300

    Добавил контроллер для игрока

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called when a move input is detected.
+    private void OnMove(InputValue movementValue)
     {
-        
+        var movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        var movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
     }
 }
